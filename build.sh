ace881fa0227dbe62f6df9761b2057f17919cadc
Matthew Mattox <mmattox@support.tools>
2022-01-25 19:39:48 -0600

    Removing sudo from gh cli

--- build.sh
+++ build.sh
@@ -30,7 +30,7 @@ helm version || exit 2
 
 echo "Installing GitHub CLI..."
 curl -fsSL https://cli.github.com/packages/githubcli-archive-keyring.gpg | sudo dd of=/etc/apt/trusted.gpg.d/githubcli-archive-keyring.gpg
-echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/trusted.gpg.d/githubcli-archive-keyring.gpg] https://cli.github.com/packages stable main" | sudo tee /etc/apt/sources.list.d/github-cli.list > /dev/null
+echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/trusted.gpg.d/githubcli-archive-keyring.gpg] https://cli.github.com/packages stable main" | tee /etc/apt/sources.list.d/github-cli.list > /dev/null
 apt update
 apt-get install -yq --no-install-recommends gh
 gh version || exit 2
