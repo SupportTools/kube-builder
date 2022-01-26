39229fc074dc768c4ecbd9015bceae79853fc68b
Matthew Mattox <mmattox@support.tools>
2022-01-25 19:43:15 -0600

    Fixing missing key

--- build.sh
+++ build.sh
@@ -29,7 +29,7 @@ chmod +x get_helm.sh
 helm version || exit 2
 
 echo "Installing GitHub CLI..."
-curl -fsSL https://cli.github.com/packages/githubcli-archive-keyring.gpg | sudo dd of=/etc/apt/trusted.gpg.d/githubcli-archive-keyring.gpg
+curl -fsSL -o /etc/apt/trusted.gpg.d/githubcli-archive-keyring.gpg https://cli.github.com/packages/githubcli-archive-keyring.gpg
 echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/trusted.gpg.d/githubcli-archive-keyring.gpg] https://cli.github.com/packages stable main" | tee /etc/apt/sources.list.d/github-cli.list > /dev/null
 apt update
 apt-get install -yq --no-install-recommends gh
